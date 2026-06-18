6bd32cf33e427a446399b33bde62be257d266e0e
SoftwareEngineeringInnovator <fredychilito@outlook.com>
2026-06-18 16:44:48 -0400

    add route to create comments

--- routes/comments.js
+++ routes/comments.js
@@ -8,9 +8,31 @@ router
   .route("/")
   .get((req, res) => {
     // GET /api/comments. Returns all comments, based on lab the array is empty
-    res.json({
-      comments,
+    res.json({comments,
+
     });
+  })
+  
+  .post((req, res, next) => {
+    // POST /api/comments, helps to create a comment - user input: userId, postId, and body.
+
+    if (req.body.userId && req.body.postId && req.body.body) {
+      const comment = {
+        // If comments is empty, start the first id at 1.
+        // If comments already has data, add 1 to the last comment id.
+        id: comments.length ? comments[comments.length - 1].id + 1 : 1,
+        userId: Number(req.body.userId),
+        postId: Number(req.body.postId),
+        body: req.body.body,
+      };
+
+      comments.push(comment);
+
+      // Send back the new comment so we can confirm it was created.
+      res.status(201).json(comment);
+    } else {
+      next(error(400, "Insufficient Data"));
+    }
   });
 
 export default router;
