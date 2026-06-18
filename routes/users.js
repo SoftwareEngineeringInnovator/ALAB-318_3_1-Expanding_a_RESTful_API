1549b7b1161c2183b0ad263413134c41e3ffd303
SoftwareEngineeringInnovator <fredychilito@outlook.com>
2026-06-17 20:28:09 -0400

    add route to get the posts by user id

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ const router = express.Router()
 
 import users from '../data/users.js'
 import error from '../utilities/error.js'
+import posts from '../data/posts.js' // This import will alow user to look inside the post array
 
 router
   .route("/")
@@ -34,6 +35,31 @@ router
       res.json(users[users.length - 1]);
     } else next(error(400, "Insufficient Data"));
   });
+
+// GET /api/users/:id/posts => Route to get all posts created by one a user
+// Example: /api/users/1/posts?api-key=perscholas
+router.get("/:id/posts", (req, res, next) => {
+  
+  // the URL is /api/users/1/posts, then req.params.id is "1".
+  const userId = Number(req.params.id);
+
+  // Check if the user exists.
+  const user = users.find((u) => u.id === userId);
+
+  // If the user does not exist, send the request to the 404 error handler.
+  if (!user) {
+    return next(error(404, "User Not Found"));
+  }
+
+  // Find all posts where the post's userId matches the user id from the URL.
+  const userPosts = posts.filter((post) => post.userId === userId);
+
+  // Send back the user and their posts.
+  res.json({
+    user,
+    posts: userPosts,
+  });
+});
 
 router
   .route("/:id")
